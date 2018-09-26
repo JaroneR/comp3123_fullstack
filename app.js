bb0fc14997c74b13ecdfe3013e7d56a53c43e05d
JaroneR <37225409+JaroneR@users.noreply.github.com>
2018-09-26 11:29:41 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+
+function greet(){
+    console.log("Hello World");
+}
+
+greet();
---
+++ ex1.js
@@ -0,0 +1,8 @@
+function greet(){
+    for(x = 0; x < 10; x++){
+        console.log("Hello World");
+    }
+    
+}
+
+greet();
